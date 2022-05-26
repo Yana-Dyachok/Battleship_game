1d37e5e5019dd8ce21dbae587b4c58759976d707
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-05-26 12:49:34 +0300

    Update Battleship_game.cpp

--- Battleship_game/Battleship_game.cpp
+++ Battleship_game/Battleship_game.cpp
@@ -1,10 +1,27 @@
 ﻿#include <iostream>
 #include "Battleship.h"
+#include <conio.h>
 using namespace std;
 int main()
 {
 	srand(time(NULL));
-	Battleship A;
-	A.placingShips();
-	A.hittingTarget();
+	Battleship I, Enemy;
+	Enemy.placingShips();
+	I.placingShips();
+	cout << "Hi!Welcome to Battleship. Let's play a game" << endl;
+	while (true) {
+		cout << "\tMY SHIP" << endl;
+		I.printMapEnemy();
+		I.hittingTargetEnemy();
+		cout << "\tENEMY SHIP" << endl;
+		Enemy.printMap();
+		Enemy.hittingTarget();
+		system("cls");
+		
+		if (I.getSum() == 0)break;
+		if (Enemy.getSum() == 0)break;
+	}
+	if (I.getSum() == 0)cout << "Congratulations!!You win!!!!!!!!" << endl;
+	if (Enemy.getSum() == 0)cout << "You lost!!!!!!!!" << endl;
+	cout << "Game over" << endl;
 }
