ae5bc5a4fdc14f3405e32044c190e8a7c7748e0b
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-05-26 12:47:48 +0300

    Update Battleship.h

--- Battleship_game/Battleship.h
+++ Battleship_game/Battleship.h
@@ -1,23 +1,29 @@
 #pragma once
 class Battleship
-{ private:
+{
+private:
 	int x;
 	int y;
 	char** map;
+	char** mapE;
 	int n;
-	int sum;
-  public:
-	  int getX() { return this->x; }
-	  int getY() { return this->y; }
-	  int getN() { return this->n; }
-	  int getSum() { return this->sum; }
-	  void setX(int x) { this->x = x;}
-	  void setY(int y) { this->y = y;}
-	  void setN(int n) { this->n = n; }
-	  void setSum(int sum) { this->sum = sum; }
-	  Battleship();
-	  void printMap();
-	  void placingShips();
-	  void hittingTarget();
-	  ~Battleship();
+	int sum, sum1;
+public:
+	int getX() { return this->x; }
+	int getY() { return this->y; }
+	int getN() { return this->n; }
+	int getSum() { return this->sum; }
+	int getSum1() { return this->sum1; }
+	void setX(int x) { this->x = x; }
+	void setY(int y) { this->y = y; }
+	void setN(int n) { this->n = n; }
+	void setSum(int sum) { this->sum = sum; }
+	void setSum1(int sum1) { this->sum1 = sum1; }
+	Battleship();
+	void printMap();
+	void printMapEnemy();
+	void placingShips();
+	void hittingTarget();
+	void hittingTargetEnemy();
+	~Battleship();
 };
